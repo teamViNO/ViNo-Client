d49c0663fb6b83f4434d1fa322eacc557c746f60
gs0428 <lider28@naver.com>
2024-01-07 22:30:03 +0900

    refactor-002: 변수들 각 컴포넌트로 이동

--- src/components/layout/footer/AboutViNO.tsx
+++ src/components/layout/footer/AboutViNO.tsx
@@ -1,10 +1,11 @@
 import * as FooterStyle from '@/styles/layout/Footer.style';
 
-interface IAboutViNOProps {
-  aboutViNOs: [string, string, string][];
-}
+const AboutViNO = () => {
+  const aboutViNOs: [string, string, string][] = [
+    ['TEAM Vi.NO', '고객센터 | 01041949853', '메일 | pm.ooodd@gmail.com'],
+    ['이용약관', '개인정보처리방침', '문의하기'],
+  ];
 
-const AboutViNO = ({ aboutViNOs }: IAboutViNOProps) => {
   return (
     <>
       {aboutViNOs.map((aboutViNO: [string, string, string], idx: number) => (
