23dfc861822963e3a382b508e8fab55d7d1dd151
KimSehyeoun <rla6170@naver.com>
2024-01-08 14:04:05 +0900

    feature-010 Not login SearchPage 기본틀

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,5 +1,49 @@
+import { Container, HashtagBox } from '@/styles/SearchPage';
+import TooltipImg from '@/assets/tooltip.svg';
+import SearchIcon from '@/assets/search.svg';
+
 const SearchPage = () => {
-  return <div>Search Page</div>;
+
+    return (
+            <Container>
+                <div className='wrap'>
+                    <div className="search">
+                        <div className="search-inner">
+                            <div className='header'>
+                            <span className='header3' style={{width: '508px', height: '58px'}}>찾고 싶은 키워드가 있나요?</span>
+                            <span className='header5'>찾고자 하는 키워드를 검색하면 관련 영상을 찾아드릴게요</span>
+                            </div>
+                            <div className='inputwrap'>
+                                <div className='input-inner'>
+                                    <div className='input'>
+                                        <img src={SearchIcon} alt='not IMG' 
+                                        style={{width: '36px', height: '36px',left: '0px', top: '0px'}}></img>
+                                        <input type='text' placeholder='검색하고 싶은 키워드를 입력해주세요'></input>
+                                    </div>
+                                    <button>Search</button>
+                                </div>
+                            </div>
+                            
+                        </div>
+                        <img src={TooltipImg} alt='not IMG'/>
+                    </div>
+
+                    <div className="hashtag">
+                            <HashtagBox># 기획</HashtagBox>
+                            <HashtagBox># 광고</HashtagBox>
+                            <HashtagBox># 마케팅</HashtagBox>
+                            <HashtagBox># 트렌드팅</HashtagBox>
+                            <HashtagBox># 기업</HashtagBox>
+                            <HashtagBox># 광고</HashtagBox>
+                            <HashtagBox># 마케팅팅</HashtagBox>
+                            <HashtagBox># 트렌드팅탕</HashtagBox>
+                            <HashtagBox># 기업팅팅탕</HashtagBox>
+                            <HashtagBox># 광고</HashtagBox>
+                    </div>
+                </div>
+            </Container>
+    );
+;
 };
 
 export default SearchPage;
---
+++ src/styles/SearchPage.ts
@@ -0,0 +1,231 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+    width : 100vw;
+    height : 100vh;
+
+    display : flex;
+    
+    justify-content: center;
+    align-items: center;
+    flex: none;
+    order: 0;
+    flex-grow: 0;
+
+    & div.wrap {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding: 0px;
+        gap: 60px;
+        
+        width: 908px;
+        height: 450px;
+        left: calc(50% - 908px/2);
+        top: 327px;
+    }
+
+    & span.header3 {
+        font-family: 'Pretendard';
+        font-style: normal;
+        font-weight: 700;
+        font-size: 36px;
+        line-height: 160%;
+        // identical to box height, or 58px
+        text-transform: capitalize;
+
+        /* gray-500 */
+        color: #1E1E1E;
+    }
+
+    & span.header5 {
+        width: 508px;
+        height: 26px;
+
+        /* Body1 */
+        font-family: 'Pretendard';
+        font-style: normal;
+        font-weight: 500;
+        font-size: 16px;
+        line-height: 160%;
+        /* identical to box height, or 26px */
+
+        /* gray-300 */
+        color: #BBBBBB;
+
+        order: 1;
+    }
+
+    & div.search {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding: 0px;
+        gap: 10px;
+
+        width: 908px;
+        height: 288px;
+    }
+
+    & div.search-inner {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        padding: 0px;
+        gap: 40px;
+
+        width: 908px;
+        height: 204px;
+    }
+
+    & div.header {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        padding: 0px;
+        gap: 8px;
+
+        width: 508px;
+        height: 92px;
+    }
+
+    & div.inputwrap {
+        display : flex;
+        flex-direction : column;
+        justify-content: center;
+        align-items: center;
+        padding: 0px;
+        gap: 20px;
+
+        width: 908px;
+        height: 72px;
+        background: #F3F3F3;
+        border-radius: 12px;
+    }
+
+    & div.inputwrap:hover {
+        box-shadow: 1px 1px 20px #ddd;
+    }
+
+    & div.input-inner {
+        display : flex;
+        justify-content : space-between;
+        width: 861.25px;
+        height: 36px;
+   }
+
+   & div.input {
+        display : flex;
+        gap : 20px;
+        width : 661.25px;
+        hieght : 36px;
+   }
+
+   & img.icon {
+        position: absolute;
+        visibility: hidden;
+        width: 36px;
+        height: 36px;
+        left: 0px;
+        top: 0px;
+   }
+
+   & input[type=text] {
+        padding: 0px;
+        gap: 20px;
+        width: 716.5px;
+        height: 36px;
+
+        order : 1;
+        background: #F3F3F3;
+        border : 0;
+        outline : none;
+
+        font-family: 'Pretendard';
+        font-style: normal;
+        font-weight: 500;
+        font-size: 20px;
+        line-height: 160%;
+
+        color: #1E1E1E;
+   }
+
+   & input::placeholder {
+        font-family: 'Pretendard';
+        font-style: normal;
+        font-weight: 500;
+        font-size: 20px;
+        line-height: 160%;
+
+        color: #BBBBBB;
+   }
+
+   & button {
+        width: 90px;
+        height: 36px;
+
+        font-family: 'Pretendard';
+        font-style: normal;
+        font-weight: 500;
+        font-size: 16px;
+        line-height: 160%;
+
+        color: #FFFFFF;
+        background: #BBBBBB;
+        border-radius: 8px;
+        order : 1;
+        border : 0;
+   }
+
+   & div.hashtag {
+        display: flex;
+        flex-direction: row;
+        flex-wrap : wrap;
+        align-items: center;
+        justify-content : center;
+        padding: 0px;
+        gap: 30px 5px;
+        
+        width: 572.52px;
+        height: 102px;
+        order: 1;
+   }
+   
+`;
+
+const HashtagBox = styled.div`
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 8px 20px;
+    gap: 12.9px;
+   
+    height: 41px;
+
+    background: #FFFFFF;
+
+    border: 1.29032px solid #E8E8E8;
+    border-radius: 8px;
+
+    font-family: 'Pretendard';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 12px;
+    line-height: 160%;
+
+    color: #787878;
+
+    &:hover {
+        box-shadow: 0.5px 0.5px 5px #ddd;
+      }
+`
+
+
+export {Container, HashtagBox};
+
+
