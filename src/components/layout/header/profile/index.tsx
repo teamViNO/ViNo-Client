c22a8f10dd3c72abf7350fcc56f2ed3606b48024
정성휘 <whistle_js@naver.com>
2024-01-17 17:30:31 +0900

    hotfix-042: 파일명으로 인한 최신화 할 수 없는 이슈

--- src/components/layout/header/profile/index.tsx
+++ src/components/layout/header/profile/index.tsx
@@ -1,4 +1,4 @@
-import * as HeaderStyle from '@/styles/layout/header/index';
+import * as HeaderStyle from '@/styles/layout/header';
 import { useEffect, useState } from 'react';
 import { createPortal } from 'react-dom';
 import BlurBackground from '../../BlurBackground';
