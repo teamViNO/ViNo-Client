c867528daa2f0e7f2ac620bfc759d5c59bcf1852
KimSehyeoun <rla6170@naver.com>
2024-01-31 11:26:35 +0900

    useNavigate 추가, searchResult Page 추가, flex 1 1 auto로 변경

--- src/pages/SearchResultPage.tsx
+++ src/pages/SearchResultPage.tsx
@@ -1,44 +1,71 @@
-import TagInput from "@/components/SearchPage/SearchComponent";
-import { useState } from "react";
+
+import { useState, useEffect } from "react";
 import SearchIcon from '@/assets/icons/search.svg?react'
-import FilterIcon from '@/assets/icons/filter.svg?react'
+
+import useBoolean from "@/hooks/useBoolean";
+import { useLocation } from "react-router-dom";
+import Styled from "@/styles/SearchResult";
+
+import TagInput from "@/components/SearchPage/SearchComponent";
+import SearchNotFound from "@/components/SearchPage/SearchNotFound";
+import SearchResultBox from "@/components/SearchPage/SearchResultBox";
 
 const SearchResult = () => {
     const [tags, setTags] = useState<string[]>([]);
     const [input, setInput] = useState('');
     const [searchType, setSearchType] = useState(true); // True : keyword | False : hashTag
+    const [isScrolling, ,startScrolling,,] = useBoolean(false);
+    const location = useLocation();
 
+    useEffect(() => {
+        const fetchData = () => {
+            try {
+                const searchParams = new URLSearchParams(location.search);
+                const type = searchParams.get('type');
+                const data = searchParams.get('value');
+
+                if (type === 'keyword' && data) {
+                    setInput(data);
+                    setSearchType(true);
+                } else if (type === 'hashtag' && data) {
+                    const initialTagList = data.split('&');
+                    setTags(initialTagList)
+                    setSearchType(false);
+                }
+            } catch (error) {
+                console.error('Error:', error);
+            }
+        };
+        fetchData();
+    }, [location])
   return (
-    <div style={{width : '100wh', height : '100vh'}}>
-        <div className="inputContainer">
-            <div className='inputwrap'>
-                    <div className='input-inner'>
-                        <div className='input'>
+    <Styled.Container style={{width : '100vw', height : '100vh'}}>
+        <div className="inputContainer" 
+        style={{
+            height: '210px',
+            boxShadow: isScrolling ? '0px 4px 40px rgba(0, 0, 0, 0.05)' : ''
+          }}>
+            <div className='inputwrap' style={{width : '908px', height : '72px'}}>
+                    <div className='input-inner' style={{width : '860px', height : '36px'}}>
+                        <div className='input' style={{width : '770px', height : '36px'}}>
                             <SearchIcon width={36} height={36}/>
                             <TagInput tags={tags} input={input} searchType={searchType}
                             setTags={setTags} setInput={setInput} setSearchType={setSearchType}/>
                         </div>
-                        <button className='search-btn' disabled={(input.length === 0 && tags.length === 0)}>Search</button>
+                        <button className='search-btn' style={{width : '90px', height : '36px'}} disabled={(input.length === 0 && tags.length === 0)}>Search</button>
                     </div>
             </div>
-            
         </div>
         
-        <div className="result">
-            <div className="filter">
+        <div className="result" onScroll={startScrolling}>
+            <div className="filter" style={{width : '910px', height : '24px'}}>
                 <span>총 0개의 영상이 발견되었어요!</span>
-                <div className="select">
-                <select>
-                    <option>최근 등록순</option>
-                </select>
-                <FilterIcon/>
-                </div>
             </div>
             <div className="content">
-
+                <SearchNotFound input="dasdd"></SearchNotFound>
             </div>
         </div>
-    </div>
+    </Styled.Container>
   );
 };
 
