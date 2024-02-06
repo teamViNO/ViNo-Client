fa0af440ededadbca5bd88c048e793bce8ff2c0f
KimSehyeoun <rla6170@naver.com>
2024-02-06 10:10:06 +0900

    api 예시 적용

--- src/pages/SearchResultPage.tsx
+++ src/pages/SearchResultPage.tsx
@@ -1,5 +1,5 @@
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import SearchIcon from '@/assets/icons/search.svg?react'
 import useBoolean from "@/hooks/useBoolean";
 import { useLocation } from "react-router-dom";
@@ -7,24 +7,94 @@ import Styled from "@/styles/SearchResult";
 import TagInput from "@/components/SearchPage/SearchComponent";
 import SearchNotFound from "@/components/SearchPage/SearchNotFound";
 import { axiosInstance } from "@/apis/config/instance";
+import { escapeHTML } from "@/utils/string";
 
+
+const apiSample = {
+    isSuccess : true,
+    code : 200,
+    message : "success!",
+    result : {
+        videos : [
+            {
+            videoid : 9,
+            title : "",
+            image : "link",
+            created_at : new Date,
+            name : "",
+            content : "",
+            user : "",
+            tag : [
+                {
+                    "name": "부동산"
+                },
+                {
+                    "name": "재건축"
+                },
+                {
+                    "name": "경제"
+                },
+                {
+                    "name": "정부정책"
+                }
+            ]
+        }
+        ]
+    }
+}
 const SearchResult = () => {
     const [tags, setTags] = useState<string[]>([]);
     const [input, setInput] = useState('');
     const [searchType, setSearchType] = useState(true); // True : keyword | False : hashTag
     const [isScrolling, ,startScrolling,,] = useBoolean(false);
     const [loading, setLoading] = useState(Boolean);
-    
+    const [data, setData] = useState([]);
     const location = useLocation();
 
+    const formattedScriptList = useMemo(() => {
+        return apiSample.result.videos.map(({ content, ...others }) => {
+          if (input.trim() !== '') {
+            content = content
+              .split(input)
+              .map((s) => escapeHTML(s))
+              .join(`<mark>${escapeHTML(input)}</mark>`);
+          } else {
+            content = escapeHTML(content);
+          }
+          content = content.replace(/\n/g, '<br>');
+          return {
+            content,
+            ...others,
+          };
+        });
+      }, []);
+
     useEffect(() => {
+        const crollLoc = () => {
+            try {
+                const searchParams = new URLSearchParams(location.search);
+                const type = searchParams.get('type');
+                const data = searchParams.get('value');
+                if (type === 'keyword' && data) {
+                    setInput(data);
+                    setSearchType(true);
+                } else if (type === 'hashtag' && data) {
+                    const initialTagList = data.split('&');
+                    setTags(initialTagList)
+                    setSearchType(false);
+                }
+            } catch (error) {
+                console.error('Error:', error);
+                // 사용자에게 오류를 알리는 로직을 추가할 수 있습니다.
+            }
+        }
         const fetchApiData = async () => {
             try {
                 setLoading(true);
-                const response = await axiosInstance.get('/search/keyword', {
-                    data : {
+                const response = await axiosInstance.get('/dummies/dummyProgressFail', {
+                    /*data : {
                         "keywords" : ['dasdas']
-                    }
+                    }*/
                 })
                 setLoading(false);
                 return response.data;
@@ -33,10 +103,15 @@ const SearchResult = () => {
                 return notFound;
             }
         }
+
+        crollLoc();
         fetchApiData().then((data) => {
-            console.log(data);
+            if(apiSample.isSuccess){
+                
+            }
         });
-    }, [])
+    }, [location])
+
 
   if(loading){
     return (
