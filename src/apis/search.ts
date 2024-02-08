1dee84e7e72c53377e06386cffefc366388bc8ad
KimSehyeoun <rla6170@naver.com>
2024-02-08 23:22:49 +0900

    search api

--- src/apis/search.ts
+++ src/apis/search.ts
@@ -1,15 +1,11 @@
 import { APIResponse } from '@/models/config/axios';
 import axios from './config/instance';
-import { SearchKeywordRequest, SearchResponse } from '@/models/search';
 
 const PREFIX = '/search';
 
-export const searchKeyowrdAPI = (keyword : string) => {
-    return axios.get<APIResponse<never>>(PREFIX + '/keyword/', {
-        params : {keyName : keyword}
+export const searchAPI = (type : string, keyword : string) => {
+    const paramType = type === 'keyword' ? 'keywordName' : 'hashtagName';
+    return axios.get<APIResponse<any>>(PREFIX + `/${type}/`, {
+        params : {[paramType] : keyword}
     });
-};
-
-export const searchHashtagAPI = () => {
-    return axios.get<APIResponse<never>>(PREFIX + '/hashtag');
 }
