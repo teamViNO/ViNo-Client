ac27edcb724f00fd0f15946017dba9e15a16e222
정성휘 <whistle_js@naver.com>
2024-02-08 16:15:37 +0900

    feature-056: 빌드 오류 및 이미지 해결

--- src/components/Home/InsightVideos.tsx
+++ src/components/Home/InsightVideos.tsx
@@ -1,30 +1,29 @@
 import React from 'react';
+
 import { InsightVideosContainer } from '@/styles/HomepageStyle';
-import Card from '../category/Card';
 
 interface InsightVideosProps {
   username: string;
   popularHashtags: string[];
 }
 
-const InsightVideos: React.FC<InsightVideosProps> = ({ username, popularHashtags }) => {
-  const formattedHashtags = popularHashtags.map(tag => '#' + tag);
+const InsightVideos: React.FC<InsightVideosProps> = ({
+  username,
+  popularHashtags,
+}) => {
+  const formattedHashtags = popularHashtags.map((tag) => '#' + tag);
 
   return (
     <InsightVideosContainer>
-      <div className='insight-container'>
-        <div className='text-container'>
-          <h2 className='insight-title'>
-            이런 인사이트는 어때요?
-          </h2>
-          <h4 className='insight-subtitle'>
-            {username}님이 많이 찾은 {formattedHashtags.join(', ')} 관련 콘텐츠에요!
+      <div className="insight-container">
+        <div className="text-container">
+          <h2 className="insight-title">이런 인사이트는 어때요?</h2>
+          <h4 className="insight-subtitle">
+            {username}님이 많이 찾은 {formattedHashtags.join(', ')} 관련
+            콘텐츠에요!
           </h4>
         </div>
-        <div className='insight-videos'>
-          <Card />
-        </div>
-        
+        <div className="insight-videos">{/* <Card /> */}</div>
       </div>
     </InsightVideosContainer>
   );
--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -2,10 +2,12 @@ import * as FooterStyle from '@/styles/layout/footer';
 import AboutViNO from './AboutViNO';
 import SendEmail from './SendEmail';
 
+import FooterLogo from '@/assets/logo-footer.png';
+
 const Footer = () => {
   return (
     <FooterStyle.Container>
-      <img src="/src/assets/logo-footer.png" alt="Footer 로고 이미지" />
+      <img src={FooterLogo} alt="Footer 로고 이미지" />
       <SendEmail />
       <AboutViNO />
       <FooterStyle.CopyRight>
