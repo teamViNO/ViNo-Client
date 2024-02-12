fe5e7a4442ac177ddd7d69afa7039c1a9ab9b6b8
정성휘 <whistle_js@naver.com>
2024-02-12 22:40:18 +0900

    feature-059: 소셜 로그인 후 처리

--- src/pages/SocialAccountPage.tsx
+++ src/pages/SocialAccountPage.tsx
@@ -1,15 +1,22 @@
 import { useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
+import { useSetRecoilState } from 'recoil';
 
 import { kakaoLoginAPI } from '@/apis/social-account';
+
+import { userTokenState } from '@/stores/user';
 
 const SocialAccountPage = () => {
   const navigate = useNavigate();
   const { search } = useLocation();
+  const setUserToken = useSetRecoilState(userTokenState);
 
   const callAPI = async (code: string) => {
     try {
-      console.log(await kakaoLoginAPI(code));
+      const { token } = (await kakaoLoginAPI(code)).data.result;
+
+      setUserToken(token);
+      navigate('/');
     } catch (e) {
       console.error(e);
       navigate('/sign-in');
