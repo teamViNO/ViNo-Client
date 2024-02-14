049fddfab2ade97e3dbca7f4d9e15f7f2e94ee95
김지나 <jina4066@naver.com>
2024-02-15 01:28:00 +0900

    feature-026: 최근 읽은 영상 수정

--- src/components/Home/RecentVideos.tsx
+++ src/components/Home/RecentVideos.tsx
@@ -4,6 +4,8 @@ import {
   VideosSubtitle,
   VideosTitle,
 } from '@/styles/HomepageStyle';
+import { Link } from 'react-router-dom';
+import MoveIcon from '@/assets/icons/move.svg?react';
 import CardImage from '@/assets/empty-video.png';
 import { CardContainer } from '@/styles/category/Card.style';
 import Card from '../category/Card';
@@ -17,24 +19,35 @@ const RecentVideos = ({ videos }: IRecentVideosProp) => {
   return (
     <RecentVideosContainer>
       <div className="container">
-        <VideosTitle>최근 읽은 영상</VideosTitle>
-
+        <div className='title-container'>
+            <VideosTitle>최근 읽은 영상</VideosTitle>
+            {videos.length >= 4 && (
+                <Link to='/videos/recent'>
+                    <div className='icon-wrapper'>
+                        <MoveIcon width={28} height={28}/>
+                    </div>
+                </Link>
+            )}
+        </div>
+        
         {videos.length === 0 && (
           <>
             <div className="empty-video">
               <img src={CardImage} alt="비어있는 비디오 이미지" />
             </div>
-            <VideosSubtitle>
-              처음 방문하셨나요? <br /> 아직 정리해본 영상이 없어요!
-            </VideosSubtitle>
-            <VideoButton>
-              <h2 className="button-text">영상 정리해보기</h2>
-            </VideoButton>
+            <div className='empty-text'>
+                <VideosSubtitle>
+                처음 방문하셨나요? <br /> 아직 정리해본 영상이 없어요!
+                </VideosSubtitle>
+                <VideoButton>
+                <h2 className="button-text">영상 정리해보기</h2>
+                </VideoButton>
+            </div>
           </>
         )}
         {videos.length > 0 && (
           <CardContainer>
-            {videos.map((video) => (
+            {videos.slice(0, 3).map((video) => (
               <Card key={video.category_id} mode="default" video={video} />
             ))}
           </CardContainer>
