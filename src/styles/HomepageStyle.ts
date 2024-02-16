93197854bd7865e26a0964105b863a1b68d72f69
gs0428 <lider28@naver.com>
2024-02-16 22:51:21 +0900

    feature-070: 카테고리 별 비디오 가져오는 API 로직 수정

--- src/styles/HomepageStyle.ts
+++ src/styles/HomepageStyle.ts
@@ -232,13 +232,13 @@ export const VideoButton = styled.button`
   }
 `;
 
-export const InsightVideosContainer = styled.div<{ userToken: string | null }>`
+export const InsightVideosContainer = styled.div<{ user: string | null }>`
   display: flex;
   justify-content: center;
   background-color: ${theme.color.white};
   width: 100vw;
   border-radius: 50px 50px 0 0;
-  padding: ${(props) => (props.userToken ? '0' : '100px')} 265px 110px;
+  padding: ${(props) => (props.user ? '0' : '100px')} 265px 110px;
   position: relative;
   bottom: 50px;
 
