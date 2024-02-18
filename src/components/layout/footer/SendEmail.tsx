5acb7a05179ac1cf059409ac4ab8484807b859c4
gs0428 <lider28@naver.com>
2024-02-19 00:28:46 +0900

    feature-077: 이메일 전송 성공 시 보여지는 UI 구현

--- src/components/layout/footer/SendEmail.tsx
+++ src/components/layout/footer/SendEmail.tsx
@@ -2,10 +2,12 @@ import * as FooterStyle from '@/styles/layout/footer';
 import { useState } from 'react';
 
 import SendEmailImage from '@/assets/mail.png';
+import SuccessSendEmailImage from '@/assets/success-mail.png';
 import { postFeedback } from '@/apis/feedback';
 
 const SendEmail = () => {
   const [feedback, setFeedback] = useState<string>('');
+  const [successSend, setSuccessSend] = useState(false);
 
   const handleInputFeedback = (e: React.ChangeEvent<HTMLInputElement>) =>
     setFeedback(e.target.value);
@@ -14,27 +16,43 @@ const SendEmail = () => {
     const res = await postFeedback(feedback);
     if (res.data.success) {
       setFeedback('');
+      setSuccessSend(true);
       return;
     }
     alert('피드백을 전송하는 과정에서 오류가 발생했습니다.');
   };
 
   return (
-    <FooterStyle.SendEmailWrap>
-      <FooterStyle.SendEmailImage src={SendEmailImage} alt="메일 보내기" />
-      <FooterStyle.SendEmailInput
-        type="text"
-        placeholder="서비스를 이용하면서 불편하거나, 좋은 피드백이 있다면 보내주세요!"
-        value={feedback}
-        onChange={handleInputFeedback}
-      />
-      <FooterStyle.SendEmailButton
-        onClick={onSendFeedback}
-        disabled={!feedback}
-        className={`${!feedback && 'disabled'}`}
-      >
-        보내기
-      </FooterStyle.SendEmailButton>
+    <FooterStyle.SendEmailWrap className={`${successSend && 'success-send'}`}>
+      {successSend && (
+        <>
+          <FooterStyle.SendEmailImage
+            src={SuccessSendEmailImage}
+            alt="메일 보내기"
+          />
+          <FooterStyle.SuccessSendEmail>
+            소중한 피드백이 전달되었어요!
+          </FooterStyle.SuccessSendEmail>
+        </>
+      )}
+      {!successSend && (
+        <>
+          <FooterStyle.SendEmailImage src={SendEmailImage} alt="메일 보내기" />
+          <FooterStyle.SendEmailInput
+            type="text"
+            placeholder="서비스를 이용하면서 불편하거나, 좋은 피드백이 있다면 보내주세요!"
+            value={feedback}
+            onChange={handleInputFeedback}
+          />
+          <FooterStyle.SendEmailButton
+            onClick={onSendFeedback}
+            disabled={!feedback}
+            className={`${!feedback && 'disabled'}`}
+          >
+            보내기
+          </FooterStyle.SendEmailButton>
+        </>
+      )}
     </FooterStyle.SendEmailWrap>
   );
 };
