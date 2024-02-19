39817f0264689b23b5027924b5c1c0517f19ea41
gs0428 <lider28@naver.com>
2024-02-19 21:20:24 +0900

    feature-079: 마이페이지 스켈레톤 1차 구현

---
+++ src/components/skeleton/ProfilePageSkeleton.tsx
@@ -0,0 +1,11 @@
+import * as ProfilePageSkeletonStyles from '@/styles/skeleton/ProfilePageSkeleton.style';
+
+const ProfilePageSkeleton = () => {
+  return (
+    <ProfilePageSkeletonStyles.Container>
+      <ProfilePageSkeletonStyles.Title />
+    </ProfilePageSkeletonStyles.Container>
+  );
+};
+
+export default ProfilePageSkeleton;
---
+++ src/styles/skeleton/ProfilePageSkeleton.style.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import { CommonBackground } from './CategoryPageSkeleton.style';
+
+export const Container = styled.div`
+  width: 873px;
+`;
+
+export const Title = styled(CommonBackground)`
+  width: 90px;
+  height: 40px;
+`;
